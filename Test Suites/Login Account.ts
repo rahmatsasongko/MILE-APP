a39c01a58b910d36c095db28c7e4e747d69b03d8
Rahmat Sasongko <Rahmat Sasongko@LAPTOP-IUM21557>
2021-02-22 16:08:00 +0700

    TEST QA MILE APP

---
+++ Test Suites/Login Account.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Account</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9f157c09-aa2d-4e06-85b8-14c24907a66a</testSuiteGuid>
+   <testCaseLink>
+      <guid>e688a291-6d70-4df2-b88b-b6b0dbd1e9f5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Usename Password/TC_3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4568f738-fd67-45af-8d5f-4fe359d8bce2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Usename Password/TC_6</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5eb3cb9-4700-4e1d-9f3b-fb82c6d8e592</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Usename Password/TC_5</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>56e65261-a500-4a0b-ac74-8e502da688dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Usename Password/TC_4</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Login Organization.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Organization</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f3317715-da2c-4808-874d-7cb78f08b47f</testSuiteGuid>
+   <testCaseLink>
+      <guid>fd907563-9fcb-4ee7-a6c0-55b5bdd5d8b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Organisasi/TC_1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d837fbd2-1e96-4ee1-bdb2-68bc8dfa33a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Organisasi/TC_2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
